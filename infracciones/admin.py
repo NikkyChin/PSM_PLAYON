8ffa5af1473a60e23a322e723ad19b63de234c60
NicolasUDC <135658937+NikkyChin@users.noreply.github.com>
2026-02-02 15:11:01 -0300

    se agregaron los template listar_actas, detalle_acta, acta_form y admi.py

--- infracciones/admin.py
+++ infracciones/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Infraccion
 
-# Register your models here.
+@admin.register(Infraccion)
+class InfraccionAdmin(admin.ModelAdmin):
+    list_display = ("nro_acta", "dominio", "dni_infractor", "inspector", "retenido_playon", "creada_en")
+    search_fields = ("nro_acta", "dominio", "dni_infractor", "apellido_infractor")
+    list_filter = ("retenido_playon", "inspector")
