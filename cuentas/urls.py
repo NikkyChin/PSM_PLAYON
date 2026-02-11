88f6accfd77c7e7fc96eb4a07721e85ac9d5cc64
NicolasUDC <135658937+NikkyChin@users.noreply.github.com>
2026-02-11 11:31:04 -0300

    Se agrego la opcion de cambiar contraseña mientras el usuario esta logeado, se agregaron templates para esta opcion

--- cuentas/urls.py
+++ cuentas/urls.py
@@ -1,20 +1,11 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="cuentas/login.html"),
-        name="login",
-    ),
-
-    path(
-    "salir/",
-    views.logout_view,
-    name="logout", 
-    ),
-
+    path("login/", auth_views.LoginView.as_view(template_name="cuentas/login.html"), name="login"),
+    path("salir/",views.logout_view,name="logout"),
+    path("cambiar-password/",auth_views.PasswordChangeView.as_view(template_name="cuentas/cambiar_password.html", success_url=reverse_lazy("cambiar_password_exito")), name="cambiar_password",),
+    path("cambiar-password/exito/",auth_views.PasswordChangeDoneView.as_view(template_name="cuentas/cambiar_password_exito.html"),name="cambiar_password_exito",),
 ]
--- cuentas/views.py
+++ cuentas/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-
+# Vista para la página de inicio, que muestra diferentes opciones según el rol del usuario (inspector, encargado de playón, admin del sistema, juez).
 @login_required
 def home(request):
     grupos_usuario = set(request.user.groups.values_list("name", flat=True))
@@ -16,7 +16,7 @@ def home(request):
 
     return render(request, "cuentas/home.html", context)
 
-
+# Vista para cerrar sesion, que simplemente llama al logout de django y redirige a la pagina de login.
 def logout_view(request):
     logout(request)
     return redirect("login")
